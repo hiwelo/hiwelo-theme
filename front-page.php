02c665cc0c2779fe78009370fb68b5c7ce1f4299
Damien Senger <damien@raccoon.studio>
2017-06-09 22:36:14 +0200

    tpl: small basic content for the homepage

---
+++ front-page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Front page file
+ *
+ * PHP version 5
+ *
+ * @category    Theme
+ * @package     hiwelo-theme
+ * @author      Damien Senger <damien@raccoon.studio>
+ * @link        https://codex.wordpress.org/Template_Hierarchy
+ */
+
+namespace Hiwelo\Blog\Theme;
+
+use Timber\Timber;
+
+Timber::render('front-page.twig', Timber::get_context());
