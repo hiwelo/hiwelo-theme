b46dcb82c27102008df8570d8b01cdadf15e73c5
Damien Senger <damien@raccoon.studio>
2017-06-09 21:34:08 +0200

    tpl: Minimal files for a WordPress template

---
+++ constants.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * PHP methods constants definition file
+ *
+ * PHP version 5
+ *
+ * @category    Theme
+ * @package     hiwelo-theme
+ * @author      Damien Senger <damien@raccoon.studio>
+ */
+
+/**
+ * Raccoon plugin manifest file location
+ */
+define('RACCOON_MANIFEST_FILE', 'raccoon.yml');
---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * PHP methods calling file
+ *
+ * PHP version 5
+ *
+ * @category    Theme
+ * @package     hiwelo-theme
+ * @author      Damien Senger <damien@raccoon.studio>
+ * @link        https://codex.wordpress.org/Functions_File_Explained
+ */
+
+use Timber\Timber;
+
+/**
+ * Necessary files inclusion:
+ *  - constants definition file
+ */
+require_once 'constants.php';
+require_once 'vendor/autoload.php';
