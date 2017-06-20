815b5c58331471fe7d5d86d5fed482e658cf8698
Damien Senger <damien@raccoon.studio>
2017-06-20 20:20:28 +0200

    Theme: flush rewrite rules at each page if we are in the dev environment

--- lib/Theme.php
+++ lib/Theme.php
@@ -3,5 +3,29 @@
 namespace Hiwelo\Blog\Theme;
 
 class Theme {
+    public function __construct()
+    {
+        $this->flushRulesIfDevelopment();
+    }
 
+    /**
+     * Flush rewrite rules if we're in the development environment
+     *
+     * @return void
+     */
+    private function flushRulesIfDevelopment()
+    {
+        // early termination if there's no environment variable
+        if (!array_key_exists('WP_ENV', $_ENV)) {
+            return;
+        }
+
+        // early termination if the environment is not the development one
+        if ($_ENV['WP_ENV'] !== 'development') {
+            return;
+        }
+
+        flush_rewrite_rules();
+        return;
+    }
 }
