a0505eaf8b95464d1b2dc30acc3631fccf8e9580
Damien Senger <damien@raccoon.studio>
2017-06-20 13:11:13 +0200

    tpl: Portfolio template and custom post type installed

---
+++ page-portfolio.php
@@ -0,0 +1,23 @@
+<?php
+ /**
+  * Portfolio index page
+  *
+  * Template Name: Portfolio index
+  *
+  * PHP version 5
+  *
+  * @category    Theme
+  * @package     hiwelo-theme
+  * @author      Damien Senger <damien@raccoon.studio>
+  * @link        https://codex.wordpress.org/Template_Hierarchy
+  */
+
+ namespace Hiwelo\Blog\Theme;
+
+ use Timber\Timber;
+ use Timber\Post as TimberPost;
+
+ $context = Timber::get_context();
+ $context['post'] = new TimberPost();
+
+ Timber::render('page-portfolio.twig', $context);
