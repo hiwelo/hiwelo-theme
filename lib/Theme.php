2c9247414bda1a0a0f3d9e87b529f718f36a4052
Damien Senger <damien@raccoon.studio>
2017-06-24 20:44:48 +0200

    common: ?flush_all trigger the flush_rewrite_rules() WordPress function

--- lib/Theme.php
+++ lib/Theme.php
@@ -10,18 +10,19 @@ class Theme {
 
     /**
      * Flush rewrite rules if we're in the development environment
+     * or if someone call the ?flush_all in the URL
      *
      * @return void
      */
     private function flushRulesIfDevelopment()
     {
         // early termination if there's no environment variable
-        if (!array_key_exists('WP_ENV', $_ENV)) {
+        if (!array_key_exists('WP_ENV', $_ENV) && !isset($_GET['flush_all'])) {
             return;
         }
 
         // early termination if the environment is not the development one
-        if ($_ENV['WP_ENV'] !== 'development') {
+        if ($_ENV['WP_ENV'] !== 'development' && !isset($_GET['flush_all'])) {
             return;
         }
 
